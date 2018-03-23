b9ebc74c421d5fb4059c5740cfc9a669baf7344c
Brian Grad <briangrad@Brians-MacBook-Pro-2.local>
2018-03-23 10:22:39 -0500

    html

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-server
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+var http = require("http");
+var fs = require("fs");
+
+var app = express();
+
+app.listen(3100, function(){
+    console.log('Listening on port 3100')
+});
